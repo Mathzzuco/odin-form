eb31584abaab562bfbbcee9cd8f7a26bfb63aa23
Mathzzuco <matheusmazzuco05@gmail.com>
2024-04-15 21:09:41 -0300

    finish password validation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const subBtn = document.querySelector("button");
 
-function checkpassword() {
+function checkpassword(e) {
   const password = document.getElementById("password").value;
-  const confirmpassword = document.getElementById("confirm_password").value;
-  if (password === confirmpassword) {
+  const confirmPassword = document.getElementById("confirm_password").value;
+  if (password === confirmPassword) {
     console.log(password);
-    console.log(confirmpassword);
+    console.log(confirmPassword);
+    document.getElementById("password_message").innerHTML = "";
     alert("batata");
   } else {
+    e.preventDefault();
     console.log(password);
-    console.log(confirmpassword);
+    console.log(confirmPassword);
+    document.getElementById("password_message").innerHTML =
+      "* Passwords do not match";
     alert("cenoura");
   }
 }
