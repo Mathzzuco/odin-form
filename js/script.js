e727866e37bfe96d2916d6396a3241e1b2b91f23
Mathzzuco <matheusmazzuco05@gmail.com>
2024-04-21 19:03:55 -0300

    add green leaf image

--- js/script.js
+++ js/script.js
@@ -4,17 +4,11 @@ function checkpassword(e) {
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirm_password").value;
   if (password === confirmPassword) {
-    console.log(password);
-    console.log(confirmPassword);
     document.getElementById("password_message").innerHTML = "";
-    alert("batata");
   } else {
     e.preventDefault();
-    console.log(password);
-    console.log(confirmPassword);
     document.getElementById("password_message").innerHTML =
       "* Passwords do not match";
-    alert("cenoura");
   }
 }
 
