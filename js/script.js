21ae6ffb40b00f0cb1dc5f6c237ace70eaa9a04d
Mathzzuco <matheusmazzuco05@gmail.com>
2024-04-27 16:51:40 -0300

    add box shadow for button, add container for whole page and change form validation message for password

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 const inputPassword = document.querySelectorAll('input[type="password"]');
 
-function checkpassword(e) {
+function checkpassword() {
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirm_password").value;
   if (password != "" && confirmPassword != "") {
     if (password === confirmPassword) {
-      document.getElementById("password_message").innerHTML = "&nbsp";
+      document.getElementById("password_message").innerHTML = "";
       document.getElementById("password").setCustomValidity("");
       document.getElementById("confirm_password").setCustomValidity("");
     } else {
-      document.getElementById("password").setCustomValidity("invalid");
-      document.getElementById("confirm_password").setCustomValidity("invalid");
-      e.preventDefault();
+      document
+        .getElementById("password")
+        .setCustomValidity("Passwords do not match");
+      document
+        .getElementById("confirm_password")
+        .setCustomValidity("Passwords do not match");
       document.getElementById("password_message").innerHTML =
         "* Passwords do not match";
     }
