f058bed07a371d53d77a9117252376ba28e5f380
Mathzzuco <matheusmazzuco05@gmail.com>
2024-04-26 21:57:59 -0300

    change method for checking passwords and add more border style for form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-const subBtn = document.querySelector("button");
+const inputPassword = document.querySelectorAll('input[type="password"]');
 
 function checkpassword(e) {
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirm_password").value;
-  if (password === confirmPassword) {
-    document.getElementById("password_message").innerHTML = "&nbsp";
-  } else {
-    e.preventDefault();
-    document.getElementById("password_message").innerHTML =
-      "* Passwords do not match";
+  if (password != "" && confirmPassword != "") {
+    if (password === confirmPassword) {
+      document.getElementById("password_message").innerHTML = "&nbsp";
+      document.getElementById("password").setCustomValidity("");
+      document.getElementById("confirm_password").setCustomValidity("");
+    } else {
+      document.getElementById("password").setCustomValidity("invalid");
+      document.getElementById("confirm_password").setCustomValidity("invalid");
+      e.preventDefault();
+      document.getElementById("password_message").innerHTML =
+        "* Passwords do not match";
+    }
   }
 }
 
-subBtn.addEventListener("click", checkpassword);
+inputPassword.forEach((e) => {
+  e.addEventListener("input", checkpassword);
+});
